f9b40b0dd759408859901d2467e9d02ee3fa4e64
Abdulla <hmdabdulla121@gmail.com>
2023-03-16 12:45:31 +0600

    quizeTimer function process describe briefly

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const loadQuiz = async () => {
 
 // Displaying quiz on quiz page
 const displayQuiz = (data) => {
-  console.log(data);
+  
   if (!data) {
     quizContainer.innerHTML = "";
     return;
